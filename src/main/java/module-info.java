be3522293bcd7e4fa2fad8e53c5f2670158f0a4e
tiagop23 <talexandrea@ipvc.pt>
2024-01-30 15:35:50 +0000

    Interface

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.trabpraticosalinas {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.desktop;
 
     opens com.example.trabpraticosalinas to javafx.fxml;
     exports com.example.trabpraticosalinas;
